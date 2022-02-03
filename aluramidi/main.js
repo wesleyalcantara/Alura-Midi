a73773373de3e79524c186d70aab83efb5139767
wesley <wesleybaadm@gmail.com>
2022-02-03 20:13:06 -0300

    Curso de JavaScript para Web finalizado

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,25 +1,36 @@
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+    //alert('Elemento não encontrado');
+    console.log('Elemento não encontrado ou seleor invalido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
-
-while (contador < listaDeTeclas.length) {
-
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
-
-    //console.log(idAudio);
 
     tecla.onclick = function() {
         tocaSom(idAudio);
     }
-    
-    contador = contador + 1;
 
-    //console.log(contador);
+    tecla.onkeydown = function(event) {
+
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');   
+    }
 }
+//SDG
